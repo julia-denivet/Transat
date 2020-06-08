28efd76e4793858e59d5d4723201a56e2fc3eb6a
julia-denivet <julia.denivet@laplateforme.io>
2020-06-08 17:37:28 +0200

    Modif header + Index

--- Header-Footer/header.php
+++ Header-Footer/header.php
@@ -1,20 +1,19 @@
-<section>
+<section class="section_header">
     <img class="logo" src="Medias/Logos/logo.png" alt="">
-    <p> Association Transat</p>
+    <p class="p_asso_transat"> Association Transat</p>
     <article> 
         <a href="https://www.facebook.com/TransatAsso"><img src="Medias/Logos/facebook.png" alt=""></a>
         <a href="https://www.instagram.com/associationtransat/?hl=fr"><img src="Medias/Logos/instagram.png" alt=""></a>
         <a href="https://twitter.com/Asso_Transat"><img src="Medias/Logos/twitter.png" alt=""></a>
     </article>
 </section>
-
-<header>
-    <nav>
-        <a href="">Présentation générale</a>
-        <a href="">Calendrier</a>        
-        <a href="">Ressources</a>
-        <a href="">Vide dressing</a>
-        <a href="connexion.php">Connexion</a>
-        <a href="inscription.php">Inscription</a>
+<header class="header">
+    <nav class="nav_header">
+        <a class="a_header"  href="">Présentation générale</a>
+        <a class="a_header" href="">Calendrier</a>        
+        <a class="a_header" href="">Ressources</a>
+        <a class="a_header" href="">Vide dressing</a>
+        <a class="a_header" href="admin.php">admin</a>
+        
     </nav>
 </header>
--- index.php
+++ index.php
@@ -1,23 +1,52 @@
 <html>
     <head>
-        <link rel="stylesheet" href="apparence/transat-asso.css">
-        <link href="https://fonts.googleapis.com/css2?family=Comic+Neue:wght@700&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="Apparence/transat-asso.css">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+        <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>  
+       
+       
     </head>
 
-    <body>
+    <body class="body_index">
         <?php include('Header-Footer/header.php'); ?>
-
-        <div class="agencement">
-            <div class="lateral">
-                <div class="entete">
-                    <p>NEWSLETTERS</p>
+        <main>
+            <div class="d-flex">
+                <div class="lateral col-3">
+                    <div class="entete">
+                        <p>NEWSLETTERS</p>
+                    </div>
                 </div>
-            </div>
+                <div id="carouselExampleControls" class="carousel slide col-6" data-ride="carousel">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                        <img class="img_carousel_index" src="Medias/Transat/img1.jpg" class="d-block w-100" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                        <img class="img_carousel_index" src="Medias/Transat/img2.jpg" class="d-block w-100" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                        <img class="img_carousel_index" src="Medias/Transat/img3.jpg" class="d-block w-100" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                        <img class="img_carousel_index" src="Medias/Transat/img4.jpg" class="d-block w-100" alt="...">
+                        </div>
+                    </div>
+                    <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
+                </div>
 
-            <div class="contenupresentation">
-                <p>blabla bla</p>
+
             </div>
-        </div>
+        </main>
+        
         <?php include('Header-Footer/footer.php'); ?>
     </body>
 </html>
