b301eb17c1fb4b9370c44f6c865cfa14a7475437
julia-denivet <julia.denivet@laplateforme.io>
2020-06-24 18:03:08 +0200

    Commit debut php autpcompletion transat 24/06/2020

---
+++ autocompletiontransat.php
@@ -0,0 +1,14 @@
+<?php
+ if (!empty($_GET['str'])) 
+ {
+    $connexion = mysqli_connect('localhost', 'root','', 'transat');
+    mysqli_set_charset($connexion, "utf8");
+    $sql = "SELECT * FROM article WHERE titre  like '%".$_GET['srt']."%' UNION SELECT * FROM ressources WHERE titre like '%".$_GET['srt']."%'"; 
+    $query = mysqli_query($connexion,$sql);
+    while ($data = mysqli_fetch_assoc($query)) 
+    {
+       
+    }
+ }
+
+?>
