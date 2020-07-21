998b2629295bd08c3c280a96b1189a53dfd572b0
julia-denivet <julia.denivet@laplateforme.io>
2020-07-21 15:18:38 +0200

    Index CSS + JQuery

--- index.php
+++ index.php
@@ -9,11 +9,15 @@ $resc=conseille($_SESSION['user'],'ressources');
     <head>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
         <link rel="stylesheet" href="Apparence/transat-asso.css">
-        <script type="text/javascript" src="Apparence/autocompletiontransat.js" async="true"></script> 
+        <script type="text/javascript" src="Apparence/autocompletiontransat.js" async="true"></script>
+        
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
         <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
         <script type="text/javascript" src="Apparence/index.js" async="true"></script> 
+        
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
        
        
     </head>
@@ -71,37 +75,23 @@ $resc=conseille($_SESSION['user'],'ressources');
 
                             </div>     
                 </div>
-                    
+                src="" 
                 </div>
-                    <div id="carouselExampleControls" class="carousel slide col-8" data-ride="carousel">
-                        <div class="carousel-inner">
-                            <div class="carousel-item active">
-                                <img class="img_carousel_index" src="Medias/Transat/img1.jpg" class="d-block w-100" alt="...">
-                            </div>
-                            <div class="carousel-item">
-                                <img class="img_carousel_index" src="Medias/Transat/img2.jpg" class="d-block w-100"  alt="...">
-                            </div>
-                            <div class="carousel-item">
-                                <img class="img_carousel_index" src="Medias/Transat/img3.jpg" class="d-block w-100"  alt="...">
-                            </div>
-                        </div>
-                        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                            <span class="sr-only">Previous</span>
-                        </a>
-                        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                            <span class="sr-only">Next</span>
-                        </a>
-                    </div>   
-                
-                           
-                
+                <div class="slideshow">
+                    <ul>
+                        <li><img src="Medias/Transat/img1.jpg" alt="" width="350" height="200"  /></li>
+	                    <li><img src="Medias/Transat/img2.jpg" alt="" width="350" height="200"  /></li>
+	                    <li><img src="Medias/Transat/img3.jpg" alt="" width="350" height="200"  /></li>
+                    </ul>
+                </div>
+
+
+
                        
             </div>    
                                 
                     
-                
+            <script type="text/javascript" src="Apparence/carousel.js" async="true"></script>
         </main>
         <?php include('Header-Footer/footer.php'); ?>
     </body>
