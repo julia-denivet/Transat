cb47d8d5a533c09a783a8fb61ac2331c0c08e8c4
julia-denivet <julia.denivet@laplateforme.io>
2020-09-14 15:47:52 +0200

    tkt j'ai rien change

--- Apparence/carousel.js
+++ Apparence/carousel.js
@@ -1,10 +1,15 @@
 $(function(){
-    setInterval(function(){ //On vérifie que la fonctin est bien chargé prends en paramètre une fonction anonyme et le délai en millisecondes 
+    setInterval(function(){ 
        $(".slideshow ul").animate({marginLeft:-1145},800,function()
        {
-          $(this).css({marginLeft:0}).find("li:last").after($(this).find("li:first")); //je selectionne l'element ul je deplace l'element avec une animation vers la gauche avec une marge négative en 0,8s et je vais y recuperer mes elements li find() retourne aun objet jQuery puis je leur met un style particulier 
+          $(this).css({marginLeft:0}).find("li:last").after($(this).find("li:first")); 
        })
     }, 3500);
-    //animate() est une fonction callback qui permet de retouner "complete" lorsque l'animation est terminée 
-    // l"élément this représente chacune des .slideshow ul on lui supprime le margin left, puis on recupère le dernier li a l'aide de la fonction find() et ensuite on utilise la fonction after() pour recuperer le premier elements de la liste. 
+    
+    
  });
+
+ //On vérifie que la fonctin est bien chargé prends en paramètre une fonction anonyme et le délai en millisecondes 
+ //je selectionne l'element ul je deplace l'element avec une animation vers la gauche avec une marge négative en 0,8s et je vais y recuperer mes elements li find() retourne aun objet jQuery puis je leur met un style particulier 
+ //animate() est une fonction callback qui permet de retouner "complete" lorsque l'animation est terminée 
+ // l"élément this représente chacune des .slideshow ul on lui supprime le margin left, puis on recupère le dernier li a l'aide de la fonction find() et ensuite on utilise la fonction after() pour recuperer le premier elements de la liste. s
